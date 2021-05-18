db13a3ccf4fd1b78a17481d1bf67b38b26dabd5c
junaid-1995 <junaid.gondal1995@gmail.com>
2021-05-18 11:30:21 +0200

    first commit
    i'm trying using git for this project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+const [countries, setCountry] = useState([])
+const [search, setSearch] = useState('')
+
+const hook = () => {
+  axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+  const countriesData = response.data
+  console.log(countriesData)
+  setCountry(countriesData)
+}) 
+}
+
+useEffect(hook, [])
+
+const handleChange = (event) => {
+  setSearch(event.target.value)
+}
+
+const countriesToShow = countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase()))
+console.log(countriesToShow)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Search Country: <input value={search} name="search" onChange={handleChange} />
+      {countriesToShow.map(country => { return country.name})}
+      {countriesToShow.map(country => {return country.capital})}
     </div>
   );
 }
